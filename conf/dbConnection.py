34ad540c6107b8ddc2ca6c0e4b72e86dd4011fe2
LiuYaHui <gwillow@users.noreply.github.com>
2017-07-17 22:46:23 +0800

    Create dbConnection.py

---
+++ conf/dbConnection.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#coding:utf-8
+import cx_Oracle as orcl
+
+dbuser = 'pyopsd'
+dbpass = 'pyops+pk#123!'
+dbhost = '10.128.xxx.xxx'
+dbport = '3436'
+dbsid = 'rmchkpr1'
+seqkey = 'pkoss#9876543210'
+#tns=orcl.makedsn(dbhost,dbport,dbsid)
